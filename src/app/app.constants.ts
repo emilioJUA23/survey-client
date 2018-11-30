54d27670736b5c07fa7e89ca4df224cb43825fec
djob195 <djorellanab@outlook.com>
2018-11-30 14:10:16 -0600

    fix configures to nginx

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,7 +1,7 @@
 import{ SHA256} from 'crypto-js/sha256';
 
 export class AppConstants {
-    public static get baseURL(): string { return "http://186.151.197.22:8080/api/v1"; }
+    public static get baseURL(): string { return "http://186.151.197.22/api/api/v1"; }
 
     public static get pageLength(): number { return 10;}
 }
